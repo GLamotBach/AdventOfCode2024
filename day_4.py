0792654d3a5007d4800eb3e072176afac0283177
Grzegorz Lamot-Bach <105417272+stockphotohacker@users.noreply.github.com>
2024-12-07 22:22:42 +0100

    Day 4 - Task 1 - Fixed

--- day_4.py
+++ day_4.py
@@ -27,8 +27,21 @@ for x in range(len(word_puzzle)):
             if x > 2:
                 if word_puzzle[x-1][y] == 'M' and word_puzzle[x-2][y] == 'A' and word_puzzle[x-3][y] == 'S':
                     word_count += 1
-print(word_count)
+            # Diagonal up left
+            if x > 2 and y > 2:
+                if word_puzzle[x-1][y-1] == 'M' and word_puzzle[x-2][y-2] == 'A' and word_puzzle[x-3][y-3] == 'S':
+                    word_count += 1
+            # Diagonal up right
+            if x > 2 and y < len(word_puzzle[x]) - 3:
+                if word_puzzle[x-1][y+1] == 'M' and word_puzzle[x-2][y+2] == 'A' and word_puzzle[x-3][y+3] == 'S':
+                    word_count += 1
+            # Diagonal down right
+            if x < len(word_puzzle) - 3 and y < len(word_puzzle[x]) - 3:
+                if word_puzzle[x+1][y+1] == 'M' and word_puzzle[x+2][y+2] == 'A' and word_puzzle[x+3][y+3] == 'S':
+                    word_count += 1
+            # Diagonal down left
+            if x < len(word_puzzle) - 3 and y > 2:
+                if word_puzzle[x+1][y-1] == 'M' and word_puzzle[x+2][y-2] == 'A' and word_puzzle[x+3][y-3] == 'S':
+                    word_count += 1
+print(f"Task 1: {word_count}")
 
-# 432 - Too low
-# 877 - Too low
-# 913 - Too low
