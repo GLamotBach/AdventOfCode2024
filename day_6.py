46757345ef7030f02d76de8942832c30ea737501
Grzegorz Lamot-Bach <105417272+stockphotohacker@users.noreply.github.com>
2024-12-13 23:29:30 +0100

    Day 6 - take 2

--- day_6.py
+++ day_6.py
@@ -28,8 +28,27 @@ def turn_right(direct):
     return direct
 
 
-def next_step(position, direct):
-    next_s = []
+def what_direction(position, n_step):
+    if position[0] > n_step[0]:
+        direct = 'up'
+    elif position[0] < n_step[0]:
+        direct = 'down'
+    elif position[1] > n_step[1]:
+        direct = 'left'
+    elif position[1] < n_step[1]:
+        direct = 'right'
+    return direct
+
+
+def obstacle_in_way(n_step, lab_map):
+    if lab_map[n_step[0]][n_step[1]] == '#':
+        return True
+    else:
+        return False
+
+
+def next_step(position, direct, lab_map):
+    next_s = [0,0]
     if direct == 'up':
         next_s[0] = position[0] - 1
         next_s[1] = position[1]
@@ -42,38 +61,35 @@ def next_step(position, direct):
     elif direct == 'left':
         next_s[0] = position[0]
         next_s[1] = position[1] - 1
+    if obstacle_in_way(next_s, lab_map):
+        direct = turn_right(direct)
+        next_s = next_step(position, direct, lab_map)
     return next_s
 
 
 def is_in_bounds(n_step, lab_map):
     size = lab_size(lab_map)
-    if 0 > n_step[0] > size[0]:
-        return True
-    elif 0 > n_step[1] > size[1]:
-        return True
-    else:
+    if 0 > n_step[0] or n_step[0] > size[0]:
         return False
-
-
-def obstacle_in_way(n_step, lab_map):
-    if lab_map[n_step[0]][n_step[1]] == '#':
-        return True
-    else:
+    if 0 > n_step[1] or n_step[1] > size[1]:
         return False
+    return True
 
 
-# def patrol(lab_map):
-#     direction = 'up'
-#     places_been = []
-#     bounds = True
-#     position = starting_point(lab_map)
-#     places_been.append(position)
-#     while bounds:
-#         next_s = next_step(position, direction)
-#         if is_in_bounds(next_s,lab_map):
-#             if obstacle_in_way(next_s, lab_map):
-#                 direction = turn_right(direction)
-#                 next_s
+def patrol(lab_map):
+    direction = 'up'
+    places_been = []
+    bounds = True
+    position = starting_point(lab_map)
+    while bounds:
+        next_s = next_step(position, direction, lab_map)
+        direction = what_direction(position, next_s)
+        places_been.append(position)
+        print(position)
+        position = next_s
+        if not is_in_bounds(next_s, lab_map):
+            bounds = False
+    return places_been
 
 
 lab_raw = open("day_6_input.txt", "r")
@@ -83,7 +99,7 @@ for l in lab_raw:
     l = l.replace("\n",'')
     lab.append(l)
 
-# direction = 'up'
-
-# print(lab_size(lab))
-# print(starting_point(lab))
+score = patrol(lab)
+print(len(score))
+unique_data = [list(x) for x in set(tuple(x) for x in score)]
+print(len(unique_data))
